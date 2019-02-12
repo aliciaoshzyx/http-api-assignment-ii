e2038b40e68f5e038de73dafc312554c9492ea6e
aliciaoshzyx <alicia.pankratz@gmail.com>
2019-02-12 14:13:23 -0500

    start workapi pt 2

---
+++ src/jsonResponses.js
@@ -0,0 +1,108 @@
+const users = {};
+
+// function to respond with a json object
+// takes request, response, status code and object to send
+const respondJSON = (request, response, status, object) => {
+  // object for our headers
+  // Content-Type for json
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  // send response with json object
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// function to respond without json body
+// takes request, response and status code
+const respondJSONMeta = (request, response, status) => {
+  // object for our headers
+  // Content-Type for json
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  // send response without json object, just headers
+  response.writeHead(status, headers);
+  response.end();
+};
+
+// get user object
+// should calculate a 200
+const getUsers = (request, response) => {
+  // json object to send
+  const responseJSON = {
+    message: 'Success',
+    users,
+  };
+
+  // return 200 with message
+  return respondJSON(request, response, 200, responseJSON);
+};
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+
+const addUser = (request, response, body) => {
+  // default json message if left blank
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  // check if any parameters are missing
+  if (!body.name || !body.age) {
+    responseJSON.id = 'Bad Request';
+    // return 400 error
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  // default created
+  let responseCode = 201;
+
+  // check if just update
+  if (users[body.name]) {
+    console.log('in same check');
+    responseJSON.message = 'Updated (no content)';
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+  console.log(responseJSON);
+  return respondJSON(request, response, responseCode, responseJSON);
+};
+
+// function for 404 not found requests with message
+const notReal = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'Resource Not Found',
+  };
+
+  // return a 404 with an error message
+  respondJSON(request, response, 404, responseJSON);
+};
+
+// function for 404 not found without message
+const notRealMeta = (request, response) => {
+  // return a 404 without an error message
+  respondJSONMeta(request, response, 404);
+};
+
+// set public modules
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  addUser,
+  notReal,
+  notRealMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,94 @@
+const http = require('http'); // pull in http module
+// parsing url string
+const url = require('url');
+const query = require('querystring');
+// files I have made
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// hold the different url possiblitities
+/* const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/getUsers': jsonHandler.getUsers,
+  '/notReal': jsonHandler.notReal,
+  '/addUser': jsonHandler.addUser,
+  index: jsonHandler.notReal,
+};
+*/
+// when server gets a requesr
+const onRequest = (request, response) => {
+  // parse url into individual parts and return those
+  const parsedUrl = url.parse(request.url);
+
+  switch (request.method) {
+    case 'GET':
+      if (parsedUrl.pathname === '/') {
+        // if homepage, send index
+        htmlHandler.getIndex(request, response);
+      } else if (parsedUrl.pathname === '/style.css') {
+        // if stylesheet, send stylesheet
+        htmlHandler.getCSS(request, response);
+      } else if (parsedUrl.pathname === '/getUsers') {
+        // if get users, send user object back
+        jsonHandler.getUsers(request, response);
+      } else {
+        // if not found, send 404 message
+        jsonHandler.notReal(request, response);
+      }
+      break;
+    case 'HEAD':
+      if (parsedUrl.pathname === '/getUsers') {
+        // if get users, send meta data back
+        jsonHandler.getUsersMeta(request, response);
+      } else {
+        // if not found send 404 without body
+        jsonHandler.notRealMeta(request, response);
+      }
+      break;
+    case 'POST':
+      if (parsedUrl.pathname === '/addUser') {
+        const res = response;
+        const body = [];
+        console.log('in case post in server');
+        // if the upload stream errors out, just throw a
+        // a bad request and send it back
+        request.on('error', (err) => {
+          console.dir(err);
+          res.statusCode = 400;
+          res.end();
+        });
+
+        // on 'data' is for each byte of data that comes in
+        // from the upload. We will add it to our byte array.
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        });
+
+        // on end of upload stream.
+        request.on('end', () => {
+          // combine our byte array (using Buffer.concat)
+          // and convert it to a string value (in this instance)
+          const bodyString = Buffer.concat(body).toString();
+          // since we are getting x-www-form-urlencoded data
+          // the format will be the same as querystrings
+          // Parse the string into an object by field name
+          const bodyParams = query.parse(bodyString);
+
+          // pass to our addUser function
+          jsonHandler.addUser(request, res, bodyParams);
+        });
+      } else {
+        jsonHandler.notReal(request, response);
+      }
+      break;
+    default:
+      jsonHandler.notReal(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
